fd6005a148d4a6647697207c9c9312e1753a22f8
RapliVX <raffikun1203@gmail.com>
2026-03-16 12:42:29 +0700

    Revert "lib/idr.c: remove simple_ida_lock"
    This reverts commit 3a20acfccf584c58c09d532040277d72a74be900.

--- lib/idr.c
+++ lib/idr.c
@@ -3,9 +3,9 @@
 #include <linux/idr.h>
 #include <linux/slab.h>
 #include <linux/spinlock.h>
-#include <linux/xarray.h>
 
 DEFINE_PER_CPU(struct ida_bitmap *, ida_bitmap);
+static DEFINE_SPINLOCK(simple_ida_lock);
 
 /**
  * idr_alloc_u32() - Allocate an ID.
@@ -506,7 +506,7 @@ again:
 	if (!ida_pre_get(ida, gfp_mask))
 		return -ENOMEM;
 
-	xa_lock_irqsave(&ida->ida_rt, flags);
+	spin_lock_irqsave(&simple_ida_lock, flags);
 	ret = ida_get_new_above(ida, start, &id);
 	if (!ret) {
 		if (id > max) {
@@ -516,7 +516,7 @@ again:
 			ret = id;
 		}
 	}
-	xa_unlock_irqrestore(&ida->ida_rt, flags);
+	spin_unlock_irqrestore(&simple_ida_lock, flags);
 
 	if (unlikely(ret == -EAGAIN))
 		goto again;
@@ -542,8 +542,8 @@ void ida_simple_remove(struct ida *ida, unsigned int id)
 	if ((int)id < 0)
 		return;
 
-	xa_lock_irqsave(&ida->ida_rt, flags);
+	spin_lock_irqsave(&simple_ida_lock, flags);
 	ida_remove(ida, id);
-	xa_unlock_irqrestore(&ida->ida_rt, flags);
+	spin_unlock_irqrestore(&simple_ida_lock, flags);
 }
 EXPORT_SYMBOL(ida_simple_remove);
